9143240c54021baab682993cc91df2dec4870b04
FcoJAH <alcalaherrerafcoj@gmail.com>
2025-05-27 10:59:05 -0600

    Primer commit - proyecto base ExamenZeus

---
+++ src/main/java/com/examen/zeuz/ZeuzApplication.java
@@ -0,0 +1,13 @@
+package com.examen.zeuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeuzApplication.class, args);
+	}
+
+}
